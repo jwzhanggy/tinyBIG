a7ff02c6a3a0b1b13f48f8ecc7371e20407fbf18
jwzhanggy <jwzhanggy@gmail.com>
2024-06-28 13:24:55 -0700

    Commit with initial documentations created by MkDocs

--- tinybig/metric/__init__.py
+++ tinybig/metric/__init__.py
@@ -1,3 +1,14 @@
+
+"""
+This module provides the evaluation metrics that can be used with the {{our}} model.
+
+The module contains the following metric classes:
+- metric: The base evaluation metric defining the class template
+- accuracy: The Accuracy metric for classification task result evaluation.
+- f1: The F1 metric for classification task result evaluation.
+- mse: The MSE (Mean Squared Error) for regression task result evaluation.
+"""
+
 from tinybig.metric.metric import metric
 from tinybig.metric.classification_metrics import accuracy, f1
 from tinybig.metric.regression_metric import mse
--- tinybig/metric/metric.py
+++ tinybig/metric/metric.py
@@ -6,13 +6,78 @@ from abc import abstractmethod
 
 
 class metric:
-    def __init__(self, name='base_metric', *args, **kwargs):
+    """
+    The base class of the evaluation metrics in the {{toolkit}} toolkit.
+
+    ...
+
+    Attributes
+    ----------
+        name: str
+            name of the evaluation metric
+
+    Methods
+    ----------
+    evaluate(self, *args, **kwargs):
+        It performs the evaluation based on the inputs.
+    __call__(self, *args, **kwargs):
+        It performs the evaluation based on the inputs by calling the object directly.
+    """
+    def __init__(self, name: str = 'base_metric', *args, **kwargs):
+        """
+        Initialization function of the base metric class.
+
+        It can initialize a metric object based on the provided function parameters.
+
+        Parameters
+        ----------
+        :param name: name of the metric, with default value "base_metric"
+        :param args: other arguments.
+
+        Returns
+        ----------
+        :param kwargs: other arguments.
+        """
         self.name = name
 
     @abstractmethod
     def evaluate(self, *args, **kwargs):
+        """
+        The evaluate function of the base metric class.
+
+        It is declared to be an abstractmethod and needs to be implemented in the inherited evaluation metric classes.
+        The evaluate function accepts prediction results and ground-truth results as inputs and return the evaluation metric scores as the outputs.
+
+        Parameters
+        ----------
+        :param args: input prediction results and ground-truth results
+        :param kwargs: other inputs
+
+        Returns
+        ----------
+        :return: evaluation metric scores
+        """
         pass
 
     @abstractmethod
     def __call__(self, *args, **kwargs):
+        """
+        Reimplementation of the __call__ function.
+
+        It is declared to be an abstractmethod and needs to be implemented in the inherited evaluation metric classes.
+
+        Parameters
+        ----------
+        :param args: input prediction results and ground-truth results
+        :param kwargs: other inputs
+
+        Returns
+        ----------
+        :return: evaluation metric scores
+        """
         pass
+
+
+if __name__ == '__main__':
+    print(metric.__doc__)
+    help(metric)
