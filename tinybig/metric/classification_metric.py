84d5b064a87882298d24e92205b903d429d362a3
jwzhanggy <jwzhanggy@gmail.com>
2024-06-29 00:37:13 -0700

    Commit with documentations template created

--- tinybig/metric/classification_metric.py
+++ tinybig/metric/classification_metric.py
@@ -2,6 +2,11 @@
 # Author: Jiawei Zhang <jiawei@ifmlab.org>
 # Affiliation: IFM Lab, UC Davis
 
+"""
+The classification evaluation module.
+
+
+"""
 
 from tinybig.metric.metric import metric
 
@@ -10,11 +15,42 @@ from sklearn.metrics import accuracy_score, f1_score
 
 class accuracy(metric):
     """
-    The accuracy evaluation metric.
+    class accuracy
 
+    The accuracy evaluation metric. It inherits from the base metric class.
+
+    ...
+
+    Attributes
+    ----------
+    name: str, default = 'accuracy'
+        Name of the accuracy evaluation metric.
+    metric: object
+        The accuracy evaluation metric calculation method.
+
+    Methods
+    ----------
+    __init__
+        It performs the initialization of the evaluation metric. Its internal metric calculation method is declared to be accuracy_score from sklearn.
+
+    evaluate
+        It implements the abstract evaluate method declared in the base metric class. The method performs the evaluation based on the inputs.
+
+    __call__
+        It reimplements the abstract callable method declared in the base metric class.
 
     """
     def __init__(self, name='accuracy'):
+        """
+        The initialization method of the accuracy evaluation metric.
+
+        It initializes an accuracy evaluation metric object based on the input metric name. The method will also call the initialization method of the base class as well.
+
+        Parameters
+        ----------
+        name: str, default = 'accuracy'
+            The name of the evaluation metric.
+        """
         super().__init__(name=name)
         self.metric = accuracy_score
 
--- tinybig/metric/metric.py
+++ tinybig/metric/metric.py
@@ -11,60 +11,68 @@ class metric:
 
     The base class of the evaluation metrics in the tinyBIG toolkit.
 
+    ...
+
     Attributes
     ----------
-    name: str
-        name of the evaluation metric
+    name: str, default = 'base_metric'
+        Name of the evaluation metric.
 
     Methods
     ----------
+    __init__
+        It performs the initialization of the evaluation metric.
+
     evaluate
         It performs the evaluation based on the inputs.
 
     __call__
-        It performs the evaluation based on the inputs by calling the object directly.
+        It reimplementation the build-in callable method.
     """
     def __init__(self, name: str = 'base_metric', *args, **kwargs):
         """
-        Initialization function of the base metric class.
+        The initialization method of the base metric class.
 
-        It can initialize a metric object based on the provided function parameters.
+        It initializes a metric object based on the provided method parameters.
 
         Parameters
         ----------
-        name
+        name: str, default = 'base_metric'
             The name of the metric, with default value "base_metric".
 
         Returns
         ----------
-        The metric object.
+        object
+            The metric object.
         """
         self.name = name
 
     @abstractmethod
     def evaluate(self, *args, **kwargs):
         """
-        The evaluate function of the base metric class.
+        The evaluate method of the base metric class.
 
         It is declared to be an abstractmethod and needs to be implemented in the inherited evaluation metric classes.
-        The evaluate function accepts prediction results and ground-truth results as inputs and return the evaluation metric scores as the outputs.
+        The evaluate method accepts prediction results and ground-truth results as inputs and return the evaluation metric scores as the outputs.
 
         Returns
         ----------
-        The evaluation metric scores.
+        float | dict
+            The evaluation metric scores.
         """
         pass
 
     @abstractmethod
     def __call__(self, *args, **kwargs):
         """
-        Reimplementation of the build-in callable function.
+        Reimplementation of the build-in callable method.
 
         It is declared to be an abstractmethod and needs to be implemented in the inherited evaluation metric classes.
 
         Returns
         ----------
-        The evaluation metric scores.
+        float | dict
+            The evaluation metric scores.
         """
         pass
 
