dfa8286b26b9b3c9fe1747ed997b584fbd445220
jwzhanggy <jwzhanggy@gmail.com>
2024-10-22 00:04:14 -0700

    Commit with rpn gnn tuning

--- tinybig/koala/linear_algebra/matrix.py
+++ tinybig/koala/linear_algebra/matrix.py
@@ -190,6 +190,6 @@ def sparse_mx_to_torch_sparse_tensor(sparse_mx):
         np.vstack((sparse_mx.row, sparse_mx.col)).astype(np.int64))
     values = torch.from_numpy(sparse_mx.data)
     shape = torch.Size(sparse_mx.shape)
-    return torch.sparse.FloatTensor(indices, values, shape)
+    return torch.sparse_coo_tensor(indices, values, shape)
 
 
